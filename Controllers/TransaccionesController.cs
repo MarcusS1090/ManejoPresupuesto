a2bef1962f2a8c22596ee497f6985613477eda45
Marcus Aurelius Senju <thegladus323@gmail.com>
2025-03-25 22:26:58 -0500

    Corrigiendo el error de que no se mostraba las transacciones en el calendario

--- Controllers/TransaccionesController.cs
+++ Controllers/TransaccionesController.cs
@@ -278,29 +278,26 @@ namespace ManejoPresupuesto.Controllers
             return View();
         }
 
-       public async Task<JsonResult> ObtenerTransaccionesCalendario(DateTime start, 
-           DateTime end)
-       {
-           var usuarioId = servicioUsuarios.ObtenerUsuarioId();
-       
-           var transacciones = await repositorioTransacciones
-               .ObtenerPorUsuarioId(new ParametroObtenerTransaccionesPorUsuario
-               {
-                   UsuarioId = usuarioId,
-                   FechaInicio = start,
-                   FechaFin = end,
-               });
+       public async Task<JsonResult> ObtenerTransaccionesCalendario(DateTime start, DateTime end) 
+        {
+            var usuarioId = servicioUsuarios.ObtenerUsuarioId();
+            var transacciones = await repositorioTransacciones.ObtenerPorUsuarioId(new ParametroObtenerTransaccionesPorUsuario
+            {
+                UsuarioId = usuarioId,
+                FechaInicio = start,
+                FechaFin = end,
+            });
 
-            var eventosCalendario = transacciones.Select(transaccion => new EventoCalendario()
+            var eventosCalendario = transacciones.Select(transaccion => new EventoCalendario() 
             { 
                 Title = transaccion.Monto.ToString("N"),
-                Start = transaccion.FechaTransaccion.ToString("yyyy-MMM-dd"),
-                End = transaccion.FechaTransaccion.ToString("yyyy-MMM-dd"),
-                Color = (transaccion.TipoOperacionId == TipoOperacion.Gasto) ? "Red" : null,
+                Start = transaccion.FechaTransaccion.ToString("yyyy-MM-dd"),
+                End = transaccion.FechaTransaccion.ToString("yyyy-MM-dd"),
+                Color = transaccion.TipoOperacionId == TipoOperacion.Ingreso ? "#28a745" : "#dc3545",
             });
 
             return Json(eventosCalendario);
-       }
+        }
 
 
         [HttpPost]
