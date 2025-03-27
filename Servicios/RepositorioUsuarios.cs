23c61938d4e1f0464f0ec4dd1d6e042a73163def
Marcus Aurelius Senju <thegladus323@gmail.com>
2025-03-26 19:51:26 -0500

    Creando los metodos en usuarioController para la seguridad en el registro de usuarios, implmentando los modelos del usuario y creando dos servicios uno para guardar un repositorio de usuarios y uno para guardar el registro del usuario

---
+++ Servicios/RepositorioUsuarios.cs
@@ -0,0 +1,42 @@
+﻿using Dapper;
+using ManejoPresupuesto.Models;
+using Microsoft.Data.SqlClient;
+
+namespace ManejoPresupuesto.Servicios
+{
+    public interface IRepositorioUsuarios
+    {
+        Task<Usuario> BuscarUsuarioPorEmail(string emailNormalizado);
+        Task<int> CrearUsuario(Usuario usuario);
+    }
+    public class RepositorioUsuarios: IRepositorioUsuarios
+    {
+        private readonly string connectiongString;
+        
+        public RepositorioUsuarios(IConfiguration configuration)
+        {
+            connectiongString = configuration.GetConnectionString("DefaultConnection");
+        }
+
+        public async Task<int> CrearUsuario(Usuario usuario)
+        {
+            using var connection = new SqlConnection(connectiongString);
+            var id = await connection.QuerySingleAsync<int>(@"
+                    INSERT INTO Usuarios (Email,EmailNormalizado,PasswordHash) 
+                    VALUES (@Email,@EmailNormalizado,@PasswordHash);
+                    SELECT SCOPE_IDENTITY();
+                    ", usuario);
+
+            return id;
+        }
+
+        public async Task<Usuario> BuscarUsuarioPorEmail(string emailNormalizado)
+        {
+            using var connection = new SqlConnection(connectiongString);
+
+            return await connection.QuerySingleOrDefaultAsync<Usuario>(
+                "SELECT * FROM Usuarios WHERE EmailNormalizado = @emailNormalizado",
+                    new { emailNormalizado });
+        }
+    }
+}
